04dc53f0ff32d53072b58b1535ed1cf81a623788
kayphrosh <olakunbiolabode@yahoo.com>
2022-04-25 21:29:54 +0100

    changes

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-let toggleNavstatus = false;
-let getNavcontainer = document.querySelector('nav ul ')
-let getNavCon = document.querySelector('.navbar')
-let toggleNav = function() {
+// let toggleNavstatus = false;
+// let getNavcontainer = document.querySelector('nav ul ')
+// let getNavCon = document.querySelector('.navbar')
+// let toggleNav = function() {
     
 
-    if (window.innerWidth > 768 || toggleNavstatus === false) {
-        getNavcontainer.style.display = "flex";
-        getNavcontainer.style.visibility = "visible";
-        getNavCon.style.height = '100vh';
-        toggleNavstatus = true
+//     if (window.innerWidth > 768 || toggleNavstatus === false) {
+//         getNavcontainer.style.display = "flex";
+//         getNavcontainer.style.visibility = "visible";
+//         getNavCon.style.height = '100vh';
+//         toggleNavstatus = true
         
-    }
+//     }
 
-    else if (toggleNavstatus === true)  {
-        getNavcontainer.style.display =  "none";
-        getNavcontainer.style.visibility = "hidden";
-        // getNavcontainer.style.height = "0";
-        toggleNavstatus = false;
+//     else if (toggleNavstatus === true)  {
+//         getNavcontainer.style.display =  "none";
+//         getNavcontainer.style.visibility = "hidden";
+//         // getNavcontainer.style.height = "0";
+//         toggleNavstatus = false;
 
-    }
+//     }
     
-}
+// }
