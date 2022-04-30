3cd8bc015743745b21c9f84ecfc863353a65135b
kayphrosh <olakunbiolabode@yahoo.com>
2022-04-30 21:48:51 +0100

    done

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-// let toggleNavstatus = false;
-// let getNavcontainer = document.querySelector('nav ul ')
-// let getNavCon = document.querySelector('.navbar')
-// let toggleNav = function() {
+let toggleNavstatus = false;
+let getNavcontainer = document.querySelector('.nav-active')
+let getNavCon = document.querySelector('.navbar')
+let openNav = document.querySelector('#open')
+let closeNav = document.querySelector('#close')
+let toggleNav = function() {
     
 
-//     if (window.innerWidth > 768 || toggleNavstatus === false) {
-//         getNavcontainer.style.display = "flex";
-//         getNavcontainer.style.visibility = "visible";
-//         getNavCon.style.height = '100vh';
-//         toggleNavstatus = true
+    if (window.innerWidth > 768 || toggleNavstatus === false) {
+        getNavcontainer.style.display = "flex";
+        getNavcontainer.style.visibility = "visible";
+        getNavcontainer.style.minHeight = "60vh";
+        openNav.style.display = 'none'
+        closeNav.style.display = 'flex'
+        toggleNavstatus = true
         
-//     }
+    }
 
-//     else if (toggleNavstatus === true)  {
-//         getNavcontainer.style.display =  "none";
-//         getNavcontainer.style.visibility = "hidden";
-//         // getNavcontainer.style.height = "0";
-//         toggleNavstatus = false;
+    else if (toggleNavstatus === true)  {
+        getNavcontainer.style.display =  "none";
+        getNavcontainer.style.visibility = "hidden";
+        getNavcontainer.style.minHeight = "0";
+        openNav.style.display = 'flex'
+        closeNav.style.display = 'none'
+        toggleNavstatus = false;
 
-//     }
+    }
     
-// }
+}
